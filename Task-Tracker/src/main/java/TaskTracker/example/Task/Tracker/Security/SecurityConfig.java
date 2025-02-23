80986a06d2911cfc4be383e857797768959b5499
Sherrywahla <shaheryarwahla8@gmail.com>
2025-02-23 09:44:48 +0300

    update security config

--- Task-Tracker/src/main/java/TaskTracker/example/Task/Tracker/Security/SecurityConfig.java
+++ Task-Tracker/src/main/java/TaskTracker/example/Task/Tracker/Security/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
         configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
