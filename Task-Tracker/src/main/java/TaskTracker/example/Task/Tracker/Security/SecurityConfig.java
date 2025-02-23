be30c5177cefcef36b6fc235ac5baf41c0fae63e
Sherrywahla <shaheryarwahla8@gmail.com>
2025-02-23 09:33:47 +0300

    update security config

--- Task-Tracker/src/main/java/TaskTracker/example/Task/Tracker/Security/SecurityConfig.java
+++ Task-Tracker/src/main/java/TaskTracker/example/Task/Tracker/Security/SecurityConfig.java
@@ -54,11 +54,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(List.of(
-                "https://task-management-frontend-git-main-sherrywahlas-projects.vercel.app",
-                "https://task-management-frontend-be9ao9ubp-sherrywahlas-projects.vercel.app",
-                "http://localhost:3000"
-        ));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
@@ -67,6 +63,7 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 
 
 
